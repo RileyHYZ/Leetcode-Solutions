30a3e492073d05df5b9f5bcc936a91ba1fb80bca
RileyHYZ <ri.hyzhou@gmail.com>
2018-07-06 23:57:12 -0400

    upload

---
+++ Top Interview Qs - Easy/Array/reverseStr.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        // for(int i=0; i<s.size()/2;i++){
+        //     string temp = s.at(0);
+        //     s.at(0)=s.at(s.size()-n);
+        //     s.at(s.size()-n) = temp;
+        // }
+        // return s;
+        
+        string str="";
+        for(int i=s.size()-1;i>=0;i--)
+            str+=s.at(i);
+        return str;
+    }
+};
